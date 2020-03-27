8d7de5a26a14101dbcca82cc3deb93a19d8f7d22
Sergey <isb9797@gmail.com>
2020-03-27 18:32:03 +0300

    Lesson 02 complited

--- js/common.js
+++ js/common.js
@@ -1,9 +1,30 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 90000; //Доход за месяц
+let income = 'Фриланс'; //Дополнительный доход
+let addExpenses = 'Заказ блюд, Подписка на кино, Новости спорта, Музыка'; //Расходы
+let deposit = true; //Депозит
+let mission = 1000000; //Сколько накопить хочу (Да я амбициозный засранец)
+let period = 12; //За какое время
 
-alert('Первый урок на 50% выполнен');
-console.log('А теперь на 100%');
+let budgetDay = money / 30;
+
+
+console.log(typeof money, typeof income, typeof deposit);
+
+console.log(addExpenses.length + ' символа');
+
+console.log(
+    "Период равен " + period + " месяцев",
+    "\nЦель заработать " + mission + " рублей/долларов/юаней/гривен"
+);
+
+
+let addExpensesLower = addExpenses.toLowerCase();
+
+console.log(addExpensesLower.split(', '));
+
+console.log('Бюджет на день ' + budgetDay);
+
+
+
+
+
