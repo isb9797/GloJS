e2467460b5fb9d059c130ae5aff89b2b451173af
Sergey <isb9797@gmail.com>
2020-05-05 16:35:04 +0300

    Lesson 14 diff comlete

--- js/common.js
+++ js/common.js
@@ -1,12 +1,14 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize, innerTxt){
+function DomElement(possition = 'static', selector, height, width, bg, fontSize, innerTxt){
     this.selector  = selector;
     this.height  = height;
     this.width  = width;
     this.bg  = bg;
     this.fontSize = fontSize;
     this.innerTxt = innerTxt;
+    this.possition = possition;
+
 };
 
 
@@ -26,7 +28,9 @@ DomElement.prototype.addElement = function(){
           this.bg +
           ";  font-size: " +
           this.fontSize +
-          ";";
+          "; position: " +
+          this.possition +
+          ";"; 
 
         if (this.innerTxt){
             div.innerText = this.innerTxt;
@@ -52,7 +56,10 @@ DomElement.prototype.addElement = function(){
             this.bg +
             ";  font-size: " +
             this.fontSize +
-            "; display: block;";
+            "; display: block;" +
+            "position: " +
+            this.possition +
+            ";"; 
 
             if (this.innerTxt){
                 p.innerText = this.innerTxt;
@@ -65,7 +72,57 @@ DomElement.prototype.addElement = function(){
 
 
 
-const domElement = new DomElement('#my-id', '800px', '800px', 'red', '72pt', 'Привет мир!');
+window.addEventListener('DOMContentLoaded', () => {
+  const domElement = new DomElement('absolute', '.my-id', '100px', '100px', 'blue', '72pt');
 
-domElement.addElement();
+ domElement.addElement();
+ const square = document.querySelector('p, div');
+    square.style.left = 0 + 'px';
+    square.style.top = 0 + 'px';
+    let countLeft = 10;
+    let countTop = 10;
+    
+
+ document.body.addEventListener('keydown', event => {
+    const target = event.target;
+    
+    const left = () => {
+      left = countLeft;
+      countLeft+=10;
+      square.style.left = countLeft + 'px';
+    }
+
+    const right = () => {
+      console.log(square.style);
+    
+      countLeft-=10;
+      square.style.left = countLeft + 'px';
+    }
+
+    const top = () => {
+      
+      countTop-=10;
+      square.style.top = countTop + 'px';
+    }
+
+    const bottom = () => {
+      countTop+=10;
+      square.style.top = countTop + 'px';
+    }
+    //console.log('div: ', square);
+
+
+    switch(event.keyCode){
+      case 39: left();  break;
+      case 37: right(); break;
+      case 38: top(); break;
+      case 40: bottom(); break;
+    }
+ })
+
+  
+  
+})  
+
+
 
